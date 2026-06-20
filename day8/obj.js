f28f973e60297982e66531adc0b83e949c4619b4
muni <munisekhark11@gmail.com>
2026-06-20 22:19:12 +0530

    Objects.js

---
+++ day8/obj.js
@@ -0,0 +1,19 @@
+let a;
+a={
+    name: "BMW",
+    cost: 75.6,
+    mileage: 8.6,
+    start : function(){
+    console.log("Car is starting");
+   },
+   stop : function(){
+    console.log("car is stopping");
+   },
+   accelerate: function(){
+    console.log("Car is accelerating")
+   }
+
+
+};
+console.log(a.name);
+a.stop();
